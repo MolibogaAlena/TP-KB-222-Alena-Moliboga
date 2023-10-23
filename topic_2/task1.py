cb3d044d6d9a6f0e90178349d5240e8ba9ac8536
MolibogaAlena <144327044+MolibogaAlena@users.noreply.github.com>
2023-10-23 20:04:47 +0300

    task1.py

---
+++ topic_2/task1.py
@@ -0,0 +1,20 @@
+import math
+
+def solve_quadratic_equation(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root,
+    else:
+        return "Немає реальних коренів"
+
+a = float(input("Введіть коефіцієнт a: "))
+b = float(input("Введіть коефіцієнт b: "))
+c = float(input("Введіть коефіцієнт c: "))
+
+result = solve_quadratic_equation(a, b, c)
+print("Корені квадратного рівняння:", result)
