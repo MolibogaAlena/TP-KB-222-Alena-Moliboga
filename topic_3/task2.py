2e5766d33750f98b462eaf6ba6191ae78e4a5084
MolibogaAlena <144327044+MolibogaAlena@users.noreply.github.com>
2023-10-23 20:07:46 +0300

    task2.py

---
+++ topic_3/task2.py
@@ -0,0 +1,31 @@
+tx = ['a', 'l', 'e']
+tx.extend(['n', 'a'])
+print(tx)
+
+ttx = ['a', 'l', 'e', 'n']
+ttx.append('a')
+print(ttx)
+
+txx = ['a', 'l', 'e', 'n']
+txx.insert(4, 'a')
+print(txx)
+
+t = ['e', 'a', 'l', 'e', 'n', 'a']
+t.remove('e')
+print(t)
+
+X = ['a', 'l', 'e', 'n', 'a']
+X.clear()
+print(X)
+
+n = [3, 1, 2, 8, 4, 0]
+n.sort()
+print(n)
+
+
+r = ['a', 'l', 'e', 'n', 'a']
+r.reverse()
+print(r)
+
+o = ['a', 'l', 'e', 'n', 'a',]
+C = o.copy()
